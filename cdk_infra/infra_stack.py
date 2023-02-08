1e3b77c1866be111e174defe60f28bcaaedfc819
Ankit Jain <ankii.jain@gmail.com>
2023-02-08 14:23:40 +0530

    Subnet resource with basic config

--- cdk_infra/infra_stack.py
+++ cdk_infra/infra_stack.py
@@ -13,16 +13,44 @@ class InfraStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         vpc = self.__createVPC(configs.deployment_name, configs.app_parameters["vpc"])
-
+        subnets_map = self.__createSubnets(configs.deployment_name, vpc.vpc_id, configs.app_parameters["subnets"])
+        print(subnets_map)
 
     def __createVPC(self: Construct, deployment_name: str, vpc_configs: Dict) -> ec2.Vpc:
         vpc = ec2.Vpc(self, f'{deployment_name}-vpc',
                         vpc_name = f'{deployment_name}-vpc', 
                         ip_addresses = ec2.IpAddresses.cidr(vpc_configs["cidr"]),
                         enable_dns_hostnames = vpc_configs["enable_dns_hostnames"],
-                        enable_dns_support = vpc_configs["enable_dns_support"])
-
+                        enable_dns_support = vpc_configs["enable_dns_support"],
+                        subnet_configuration = [])
         CfnOutput(self, "Output",
                        value=vpc.vpc_id)
         return vpc
 
+    def __createSubnets(self: Construct, deployment_name: str, vpc_id: str, subnet_configs: Dict):
+
+        public_subnets_configs = subnet_configs["public"]
+        private_subnets_configs = subnet_configs["private"]
+        subnets_map = {}
+
+        for subnet_config in public_subnets_configs:
+
+            subnet = ec2.PublicSubnet(self, f'{deployment_name}-{subnet_config["name"]}',
+                                availability_zone=subnet_config["availability_zone"],
+                                cidr_block=subnet_config["cidr_block"],
+                                vpc_id=vpc_id,
+                                map_public_ip_on_launch=True
+                            )
+            subnets_map[subnet_config["name"]] = subnet.subnet_id
+        
+        for subnet_config in private_subnets_configs:
+
+            subnet = ec2.PrivateSubnet(self, f'{deployment_name}-{subnet_config["name"]}',
+                                availability_zone=subnet_config["availability_zone"],
+                                cidr_block=subnet_config["cidr_block"],
+                                vpc_id=vpc_id,
+                                map_public_ip_on_launch=False
+                            )
+            subnets_map[subnet_config["name"]] = subnet.subnet_id
+        
+        return subnets_map
